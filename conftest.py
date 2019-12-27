2a873f0482c26825af8b745196ecd641a137ee1c
mk01-v <prizma5150@gmail.com>
2019-12-27 18:10:20 +0700

    ---Start_work2

--- conftest.py
+++ conftest.py
@@ -12,16 +12,6 @@ from fixture.db import DbFixture
 #Для медленного запуска, сессия создается заново при инициализации теста.
 #pytest.fixture
 
-#@pytest.fixture(scope = 'session')
-#def app(request):
-#    fixture = Application()
-#    fixture.session.login(username="admin", password="secret")
-#    def fin():
-#        fixture.session.logout()
-#        fixture.destroy()
-#    request.addfinalizer(fin)
-#    return fixture
-
 fixture = None
 target = None
 
@@ -64,39 +54,6 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="Firefox")
     parser.addoption("--target", action="store", default="target.json")
-    parser.addoption("--check_ui", action="store_true")
-
-# в test_add_group проверяется наименование, в зависимости от выбранных условий подставляются данные.
-def pytest_generate_tests(metafunc):
-    for fixture in metafunc.fixturenames:
-        if fixture.startswith("data_"):
-            testdata = load_from_module(fixture[5:])
-            # 1 - куда будут подставляться параметры, 2 - какие значения подставляем, 3 - строковое представление.
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-        elif fixture.startswith("json_"):
-            testdata = load_from_json(fixture[5:])
-            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
-
-def load_from_module(module):
-    return importlib.import_module("data.%s" % module).testdata
-
-def load_from_json(file):
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/%s.json" % file)) as f:
-        return jsonpickle.decode(f.read())
-
-# фикстура для БД, запуск из файла.
-@pytest.fixture(scope="session")
-def db(request):
-    db_config = load_config(request.config.getoption("--target"))['db']
-    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
-    def fin():
-        dbfixture.destroy()
-        request.addfinalizer(fin)
-    return dbfixture
-
-@pytest.fixture
-def check_ui(request):
-    return request.config.getoption("--check_ui")
 
 
 
