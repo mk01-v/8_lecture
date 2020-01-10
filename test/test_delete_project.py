30ef6231fec8ca1148d1b9fa75c749fa354ebeab
mk01-v <prizma5150@gmail.com>
2020-01-10 18:25:58 +0700

    Тест удаление_1

---
+++ test/test_delete_project.py
@@ -0,0 +1,17 @@
+from model.project import Project
+
+
+def test_new_project(app):
+    # вход в аккаунт
+    # ввод логина, пароля, нажатие кнопки вход.
+    username = "administrator"
+    password = "root"
+    app.session.login(username, password)
+
+    # удаление проекта
+    project = Project(name="namepr1", description="namedisc1")
+    app.project.create_project(project)
+
+    # проверка проекта создания проекта
+    # assert ****
+
