241c94e07fa05bd56c975211f8aa76a2a81da601
mk01-v <prizma5150@gmail.com>
2020-01-10 17:49:43 +0700

    Тест создание проекта.

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,12 +1,13 @@
-import string
-import random
-
-def random_username(prefix, maxlen):
-    symbols = string.ascii_letters
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+from model.project import Project
 
 def test_new_project(app):
     # вход в аккаунт
+    username = "administrator"
+    password = "root"
+    app.session.login(username, password)
+    project = Project(name="namepr1", description="namedisc1")
+    app.project.create_project(project)
+
         # ввод логина, пароля, нажатие кнопки вход.
     # создание проекта
         # Manage -> Manage Projects -> Кнопка Create New Project.
