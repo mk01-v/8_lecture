d0236aa59f393b2379f67e129cbe2669766d8db5
mk01-v <prizma5150@gmail.com>
2020-01-10 16:09:32 +0700

    Составление плана

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,6 +1,17 @@
+import string
+import random
 
+def random_username(prefix, maxlen):
+    symbols = string.ascii_letters
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-
+def test_new_project(app):
+    # вход в аккаунт
+        # ввод логина, пароля, нажатие кнопки вход.
+    # создание проекта
+        # Manage -> Manage Projects -> Кнопка Create New Project.
+            # Указание имени проекта и описания.
+    # проверка проекта создания проекта
 
 
 
