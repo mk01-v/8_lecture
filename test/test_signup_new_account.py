2bd42cdb26f38ab215e230aef8757a4a97e98063
mk01-v <prizma5150@gmail.com>
2020-01-10 13:53:44 +0700

    Запрос через SOAP

--- test/test_signup_new_account.py
+++ test/test_signup_new_account.py
@@ -2,7 +2,7 @@ import string
 import random
 
 
-#-------------------------------------------------9.4
+#-------------------------------------------------9.5
 def random_username(prefix, maxlen):
     symbols = string.ascii_letters
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
@@ -14,12 +14,26 @@ def test_signup_new_account(app):
     app.james.ensure_user_exists(username, password)
     # для пароля почтового сервера и регистрации в системе mantis
     app.signup.new_user(username, email, password)
-    app.session.login(username, password)
-    assert app.session.is_logged_in_as(username)
-    app.session.logout()
+    assert app.soap.can_login(username, password)
+
+#-------------------------------------------------9.4
+#def random_username(prefix, maxlen):
+#    symbols = string.ascii_letters
+#    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+#
+#def test_signup_new_account(app):
+#    username = random_username("user_", 10)
+#    email = username + "@localhost"
+#    password = "test"
+#    app.james.ensure_user_exists(username, password)
+#    # для пароля почтового сервера и регистрации в системе mantis
+#    app.signup.new_user(username, email, password)
+#    app.session.login(username, password)
+#    assert app.session.is_logged_in_as(username)
+#    app.session.logout()
 
 
-#------------------------------------------9.3
+#-------------------------------------------------9.3
 #def test_signup_new_account(app):
 #    username = "user1"
 #    password = "test"
