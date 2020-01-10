e081b3002436013fb29daec3cb74b92aba5d5c31
mk01-v <prizma5150@gmail.com>
2020-01-10 17:50:15 +0700

    create ProjectHelper (open_project_page, fill_project_form, create_project)

--- fixture/project.py
+++ fixture/project.py
@@ -1,6 +1,33 @@
 from model.project import Project
 
+class ProjectHelper:
 
+    def __init__(self, app):
+        self.app = app
+
+    def open_project_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("Manage").click()
+        wd.find_element_by_link_text("Manage Projects").click()
+
+    def fill_project_form(self, project):
+        wd = self.app.wd
+
+        wd.find_element_by_name("name").click()
+        wd.find_element_by_name("name").clear()
+        wd.find_element_by_name("name").send_keys(project.name)
+
+        wd.find_element_by_name("description").click()
+        wd.find_element_by_name("description").clear()
+        wd.find_element_by_name("description").send_keys(project.description)
+
+    def create_project(self, project):
+        wd = self.app.wd
+        self.open_project_page()
+        wd.find_element_by_css_selector("input[value='Create New Project']").click()
+        self.fill_project_form(project)
+        wd.find_element_by_css_selector("input[value='Add Project']").click()
+        #self.group_cache = None
 
 
 
