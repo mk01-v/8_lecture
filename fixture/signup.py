69cb2094976f0aa36fbdafef9ceef237a263fb65
mk01-v <prizma5150@gmail.com>
2020-01-10 11:27:53 +0700

    Исправление под получение сообщения.

--- fixture/signup.py
+++ fixture/signup.py
@@ -1,3 +1,5 @@
+import re
+
 
 class SignupHelper:
     def __init__(self, app):
@@ -10,16 +12,17 @@ class SignupHelper:
         wd.find_element_by_name("email").send_keys(email)
         wd.find_element_by_css_selector('input[type="submit"]').click()
 
-        mail = self.app.mail.get_mail("")
+        mail = self.app.mail.get_mail(username, password, "[MantisBT] Account registration")
         url = self.extract_confirmation_url(mail)
 
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
-        wd.find_element_by_name("password1").send_keys(password)
-        wd.find_element_by_css_selector('input[type="submit"]').click()
+        wd.find_element_by_name("password_confirm").send_keys(password)
+        wd.find_element_by_css_selector('input[value="Update User"]').click()
 
-    def extract_confirmation(self):
-        pass
+    def extract_confirmation_url(self, text):
+        # .*$ - какое кол-во символов до конца строки.
+        return re.search("http://.*$", text).group(0)
 
 
 
