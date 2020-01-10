fe870b9d67a2587787c721438877bfbb1ca03b84
mk01-v <prizma5150@gmail.com>
2020-01-10 17:54:54 +0700

    mistakes

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -2,49 +2,16 @@ from model.project import Project
 
 def test_new_project(app):
     # вход в аккаунт
+        # ввод логина, пароля, нажатие кнопки вход.
     username = "administrator"
     password = "root"
     app.session.login(username, password)
+
+    # создание проекта
     project = Project(name="namepr1", description="namedisc1")
     app.project.create_project(project)
 
-        # ввод логина, пароля, нажатие кнопки вход.
-    # создание проекта
-        # Manage -> Manage Projects -> Кнопка Create New Project.
-            # Указание имени проекта и описания.
     # проверка проекта создания проекта
+    # assert ****
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
