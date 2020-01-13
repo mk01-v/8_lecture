fd3eff43ffa4abe442f433bdf4b21b9d0f3761d9
mk01-v <prizma5150@gmail.com>
2020-01-13 13:37:05 +0700

    assert len(old_projects) + 1 == len(new_projects) - OK

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,17 +1,13 @@
 from model.project import Project
 
 def test_new_project(app):
-    # вход в аккаунт
-        # ввод логина, пароля, нажатие кнопки вход.
     username = "administrator"
     password = "root"
     app.session.login(username, password)
 
     # создание проекта
-    project = Project(name="namepr1", description="namedisc1")
+    old_projects = app.project.get_project_list()
+    project = Project(name="name_123", description="description_123")
     app.project.create_project(project)
-
-    # проверка проекта создания проекта
-    # assert ****
-
-    
+    new_projects = app.project.get_project_list()
+    assert len(old_projects) + 1 == len(new_projects)
