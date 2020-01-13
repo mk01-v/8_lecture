c1eecfdcdc304ff777fefd9ebd84c4ba5838fc77
mk01-v <prizma5150@gmail.com>
2020-01-13 17:17:07 +0700

    проверки для удаления.

--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -3,8 +3,7 @@ import random
 
 
 def test_delete_project_without_soap(app):
-    app.session.login("administrator", "root")
-    assert app.session.is_logged_in_as("administrator")
+
     if len(app.project.get_project_list()) == 0:
         app.project.create_project(Project(name="test", description="qweqwe"))
     old_projects = app.project.get_project_list()
@@ -14,12 +13,10 @@ def test_delete_project_without_soap(app):
     assert len(old_projects) - 1 == len(new_projects)
     old_projects.remove(project)
     assert old_projects == new_projects
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
 
 def test_delete_project_with_soap(app):
-    # вход в аккаунт
-    # ввод логина, пароля, нажатие кнопки вход.
-    app.session.login("administrator", "root")
-    assert app.session.is_logged_in_as("administrator")
+
     if len(app.soap.get_project_list_soap("administrator", "root")) == 0:
         app.project.create_project(Project(name="test", description="qweqwe"))
     old_projects = app.soap.get_project_list_soap("administrator", "root")
@@ -28,4 +25,4 @@ def test_delete_project_with_soap(app):
     new_projects = app.soap.get_project_list_soap("administrator", "root")
     old_projects.remove(project)
     assert old_projects == new_projects
-
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
