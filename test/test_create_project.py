a14666afb967ad392fc34c0eb12dbb766278354d
mk01-v <prizma5150@gmail.com>
2020-01-13 16:55:25 +0700

    app.session.ensure_login - работает, при принудильнеом входе login - не работает.

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -13,14 +13,14 @@ def test_new_project_without_soap(app):
     assert len(old_projects) + 1 == len(new_projects)
 
 
-def test_new_project_with_soap(app):
+def test_new_project_load_projectjson_soap(app, json_projects):
     username = "administrator"
     password = "root"
-    app.session.login(username, password)
-
-    # создание проекта
-    old_projects = app.project.get_project_list()
-    project = Project(name="name_123", description="description_123")
-    app.project.create_project(project)
-    new_projects = app.project.get_project_list()
+    #app.session.login(username, password)
+    app.session.ensure_login(username, password)
+    project_json = json_projects
+    old_projects = app.soap.get_project_list_soap(username, password)
+    app.project.create_project(project_json)
+    new_projects = app.soap.get_project_list_soap(username, password)
     assert len(old_projects) + 1 == len(new_projects)
+
