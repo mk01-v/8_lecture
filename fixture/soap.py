ac9134ef2af6e824207603cf8571c69809176faf
mk01-v <prizma5150@gmail.com>
2020-01-13 11:37:58 +0700

    Список из SOAP

--- fixture/soap.py
+++ fixture/soap.py
@@ -1,6 +1,7 @@
 
 from suds.client import Client
 from suds import WebFault
+from model.project import Project
 
 class SoapHelper:
 
@@ -14,3 +15,14 @@ class SoapHelper:
             return True
         except WebFault:
             return False
+
+    def get_project_list(self, username, password):
+        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        projects = client.service.mc_projects_get_user_accessible(username, password)
+        list = []
+        for project in projects:
+            name = project.name
+            description = project.description
+            id = project.id
+            list.append(Project(name=name, description=description, id=id))
+        return list
