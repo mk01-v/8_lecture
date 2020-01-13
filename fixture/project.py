bf1f81d8b262c723212e5046468ee81d4c1bd302
mk01-v <prizma5150@gmail.com>
2020-01-13 11:37:30 +0700

    удаление по id

--- fixture/project.py
+++ fixture/project.py
@@ -27,39 +27,18 @@ class ProjectHelper:
         wd.find_element_by_css_selector("input[value='Create New Project']").click()
         self.fill_project_form(project)
         wd.find_element_by_css_selector("input[value='Add Project']").click()
-        #self.group_cache = None
+        self.project_cache = None
 
+    def select_project_by_id(self, id):
+        wd = self.app.wd
+        wd.find_element_by_css_selector("a[href='manage_proj_edit_page.php?project_id=%s']" % id).click()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def delete_project(self, id):
+        wd = self.app.wd
+        self.open_project_page()
+        self.select_project_by_id(id)
+        wd.find_element_by_css_selector("input[value='Delete Project']").click()
+        wd.find_element_by_css_selector("input[value='Delete Project']").click()
+        self.open_project_page()
+        # сбрасываем кэш, т.к. после операции является не валидным.
+        self.group_cache = None
