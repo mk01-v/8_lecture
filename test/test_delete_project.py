cdc4fbeefa22d498c43c933313a45c9d4b51486c
mk01-v <prizma5150@gmail.com>
2020-01-13 13:36:48 +0700

    test_delete_project_with_soap

--- test/test_delete_project.py
+++ test/test_delete_project.py
@@ -2,14 +2,28 @@ from model.project import Project
 import random
 
 
-def test_delete_project(app):
+def test_delete_project_without_soap(app):
+    app.session.login("administrator", "root")
+    assert app.session.is_logged_in_as("administrator")
+    old_projects = app.project.get_project_list()
+    project = random.choice(old_projects)
+    app.project.delete_project(project.id)
+    new_projects = app.project.get_project_list()
+    assert len(old_projects) - 1 == len(new_projects)
+    old_projects.remove(project)
+    assert old_projects == new_projects
+
+def test_delete_project_with_soap(app):
     # вход в аккаунт
     # ввод логина, пароля, нажатие кнопки вход.
     app.session.login("administrator", "root")
     assert app.session.is_logged_in_as("administrator")
-
-    old_projects = app.soap.get_project_list("administrator", "root")
+    old_projects = app.soap.get_project_list_soap("administrator", "root")
     project = random.choice(old_projects)
     app.project.delete_project(project.id)
+    new_projects = app.soap.get_project_list_soap("administrator", "root")
+    old_projects.remove(project)
+    assert old_projects == new_projects
+
 
 
