43690e86e14e94ee29b79860e17a93da21414c29
mk01-v <prizma5150@gmail.com>
2020-01-13 15:26:19 +0700

    ---

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,6 +1,19 @@
 from model.project import Project
 
-def test_new_project(app):
+def test_new_project_without_soap(app):
+    username = "administrator"
+    password = "root"
+    app.session.login(username, password)
+
+    # создание проекта
+    old_projects = app.project.get_project_list()
+    project = Project(name="name_123", description="description_123")
+    app.project.create_project(project)
+    new_projects = app.project.get_project_list()
+    assert len(old_projects) + 1 == len(new_projects)
+
+
+def test_new_project_with_soap(app):
     username = "administrator"
     password = "root"
     app.session.login(username, password)
