f917b1ac13c609492000d337acbc48a0af9e02d0
mk01-v <prizma5150@gmail.com>
2020-01-13 17:16:23 +0700

    проверки через списки

--- test/test_create_project.py
+++ test/test_create_project.py
@@ -1,26 +1,23 @@
 from model.project import Project
 
 def test_new_project_without_soap(app):
-    username = "administrator"
-    password = "root"
-    app.session.login(username, password)
 
-    # создание проекта
     old_projects = app.project.get_project_list()
     project = Project(name="name_123", description="description_123")
     app.project.create_project(project)
     new_projects = app.project.get_project_list()
     assert len(old_projects) + 1 == len(new_projects)
-
+    old_projects.append(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
 
 def test_new_project_load_projectjson_soap(app, json_projects):
     username = "administrator"
     password = "root"
-    #app.session.login(username, password)
-    app.session.ensure_login(username, password)
+
     project_json = json_projects
     old_projects = app.soap.get_project_list_soap(username, password)
     app.project.create_project(project_json)
     new_projects = app.soap.get_project_list_soap(username, password)
     assert len(old_projects) + 1 == len(new_projects)
+
 
